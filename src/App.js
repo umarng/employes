382998a705649dfe358b157fa78131087877a590
ElBachir Toure <bassirou.toure@esp.sn>
2024-01-04 19:34:49 +0000

    last

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [films, setFilms] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   useEffect(() => {
     fetch("https://reactnative.dev/movies.json")
       .then(result => result.json())
-      .then(result => setFilms(result.movies))
+      .then(result => {
+        setFilms(result.movies);
+        setTitle(result.title);
+        setDescription(result.description);
+      })
       .catch(err => console.log(err));
   });
 
   return (
     <div className="App">
+      <h2>Title = {title}</h2>
+      <h3>description = {description}</h3>
       <Movies data={films} />
     </div>
   );
